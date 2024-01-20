1d8af58ca7c1aa091e143a18bcefb1ac258c9526
Peter Delgado <peterrafaeldelgado@gmail.com>
2024-01-20 11:09:30 +0100

    Initial Commit

---
+++ TripRoute/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TripRoute
+//
+//  Created by Peter Delgado on 20/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TripRoute/TripRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripRouteApp.swift
+//  TripRoute
+//
+//  Created by Peter Delgado on 20/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TripRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
